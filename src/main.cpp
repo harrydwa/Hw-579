a304b568c2ec374ed269dedbaa476d910ce4cfef
Harryyy <harrykeut.2@gmail.com>
2025-02-18 16:58:39 +0800

    attachinterrupt trial

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,31 @@
 #include <QMC5883LCompass.h>
+#include <Arduino.h>
+#define BUTTON_PIN 4 // GPIO 4 (D2 on WEMOS D1 R32)
 
 QMC5883LCompass compass;
+volatile bool buttonPressed = false;
+unsigned long lastDebounceTime = 0;
+const unsigned long debounceDelay = 50; // 50 milliseconds debounce delay
+
+// Interrupt Service Routine (ISR)
+void IRAM_ATTR buttonISR() {
+  unsigned long currentTime = millis();
+  if ((currentTime - lastDebounceTime) > debounceDelay) {
+    buttonPressed = !buttonPressed;
+    lastDebounceTime = currentTime;
+    Serial.println("Interrupt triggered!"); // Print a message when the interrupt is triggered
+  }
+}
 
 void setup() {
-  Serial.begin(9600);
+  pinMode(BUTTON_PIN, INPUT_PULLUP); // Set GPIO 4 as input with internal pull-up resistor
+  Serial.begin(115200);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), buttonISR, CHANGE);
   compass.init();
 }
 
 void loop() {
-
-	int x, y, z, a, b;
-	char myArray[3];
-	
-	compass.read();
-  
-	x = compass.getX();
-	y = compass.getY();
-	z = compass.getZ();
-	
-	a = compass.getAzimuth();
-	
-	b = compass.getBearing(a);
-
-	compass.getDirection(myArray, a);
-  
-  
-	Serial.print("X: ");
-	Serial.print(x);
-
-	Serial.print(" Y: ");
-	Serial.print(y);
-
-	Serial.print(" Z: ");
-	Serial.print(z);
-
-	Serial.print(" Azimuth: ");
-	Serial.print(a);
-
-	Serial.print(" Bearing: ");
-	Serial.print(b);
-
-	Serial.print(" Direction: ");
-	Serial.print(myArray[0]);
-	Serial.print(myArray[1]);
-	Serial.print(myArray[2]);
-
-	Serial.println();
-
-	delay(250);
+  Serial.println(digitalRead(BUTTON_PIN)); // Print the state of the button
+  Serial.println(compass.getAzimuth());
+  delay(1000);
 }
