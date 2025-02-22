cb5f791e55e6292c2238bfc8087f1be629dd0b53
Harry <harrykeut.2@gmail.com>
2025-02-22 17:18:59 +0800

    itg3200

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,86 @@
-/*!
- * @file getSixaxis.ino
- * @brief For collecting the sensor acceleration values on the 3 axes, calculate Roll and Pitch angle information according to the data
- * @copyright   Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
- * @license     The MIT License (MIT)
- * @author      PengKaixing(kaixing.peng@dfrobot.com)
- * @version  V2.0.0
- * @date  2022-02-22
- * @url https://github.com/DFRobot/DFRobot_ADXL345
- */
+// I2C device class (I2Cdev) demonstration Arduino sketch for ITG3200 class
+// 10/7/2011 by Jeff Rowberg <jeff@rowberg.net>
+// Updates should (hopefully) always be available at https://github.com/jrowberg/i2cdevlib
+//
+// Changelog:
+//     2011-10-07 - initial release
 
- #include "DFRobot_ADXL345.h"
+/* ============================================
+I2Cdev device library code is placed under the MIT license
+Copyright (c) 2011 Jeff Rowberg
 
- #define cs_pin 10
- 
- DFRobot_ADXL345_I2C ADXL345(&Wire,0x53);
- /**
-  * Note that the ICSP pin need to be connected when using leonardo as the main controller.
-  */
- // DFRobot_ADXL345_SPI ADXL345(&SPI, cs_pin);
- 
- int accval[3];
- 
- void setup() 
- {
-   Serial.begin(9600);
-   ADXL345.begin();
-   ADXL345.powerOn();
- }
- 
- void loop() 
- {
-   ADXL345.readAccel(accval);
-   Serial.println("Triaxial acceleration value is :");
-   Serial.print("x:");Serial.print(accval[0]);
-   Serial.print(" y:");Serial.print(accval[1]);
-   Serial.print(" z:");Serial.println(accval[2]);
-   ADXL345.RPCalculate(accval);
-   Serial.print("Roll:"); Serial.println( ADXL345.RP.roll );
-   Serial.print("Pitch:"); Serial.println( ADXL345.RP.pitch );
-   delay(500);
- }
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+===============================================
+*/
+
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#include "Wire.h"
+
+// I2Cdev and ITG3200 must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of your project
+#include "I2Cdev.h"
+#include "ITG3200.h"
+
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for SparkFun 6DOF board)
+// AD0 high = 0x69 (default for SparkFun ITG-3200 standalone board)
+ITG3200 gyro;
+
+int16_t gx, gy, gz;
+
+#define LED_PIN 13
+bool blinkState = false;
+
+void setup() {
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    Wire.begin();
+
+    // initialize serial communication
+    // (38400 chosen because it works as well at 8MHz as it does at 16MHz, but
+    // it's really up to you depending on your project)
+    Serial.begin(9600);
+
+    // initialize device
+    Serial.println("Initializing I2C devices...");
+    gyro.initialize();
+
+    // verify connection
+    Serial.println("Testing device connections...");
+    Serial.println(gyro.testConnection() ? "ITG3200 connection successful" : "ITG3200 connection failed");
+
+    // configure Arduino LED pin for output
+    pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+    // read raw gyro measurements from device
+    gyro.getRotation(&gx, &gy, &gz);
+
+    // display tab-separated gyro x/y/z values
+    Serial.print("gyro:\t");
+    Serial.print(gx); Serial.print("\t");
+    Serial.print(gy); Serial.print("\t");
+    Serial.println(gz);
+
+    // blink LED to indicate activity
+    blinkState = !blinkState;
+    digitalWrite(LED_PIN, blinkState);
+}
